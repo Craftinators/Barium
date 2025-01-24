550379adac999340ac87af8660c58c926f279b05
Craftinators <craftinatorssx@gmail.com>
2025-01-23 20:01:17 -0500

    feat(JobTransfer): implements `Comparable`

--- src/main/java/me/craftinators/barium/core/JobTransfer.java
+++ src/main/java/me/craftinators/barium/core/JobTransfer.java
@@ -1,4 +1,20 @@
 package me.craftinators.barium.core;
 
-public record JobTransfer(Job oldJob, Job newJob) {
+import org.jetbrains.annotations.NotNull;
+
+public record JobTransfer(Job oldJob, Job newJob) implements Comparable<JobTransfer> {
+    /**
+     * Compares this {@link JobTransfer} with the specified {@link JobTransfer} for order.
+     * The comparison is based first on {@code oldJob}, and if they are equal, on {@code newJob}.
+     *
+     * @param other the {@link JobTransfer} to be compared
+     * @return a negative integer, zero, or a positive integer as this {@link JobTransfer}
+     *         is less than, equal to, or greater than the specified {@link JobTransfer}.
+     */
+    @Override
+    public int compareTo(@NotNull JobTransfer other) {
+        int oldJobComparison = oldJob.compareTo(other.oldJob());
+        if (oldJobComparison == 0) return newJob.compareTo(other.newJob());
+        return oldJobComparison;
+    }
 }
