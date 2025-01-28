a8dc9157b94a062750522434c8f37ad351457630
Craftinators <craftinatorssx@gmail.com>
2025-01-28 16:41:09 -0500

    feat(JobTransfer): add `JobTransfer#isRedundant` method

--- src/main/java/me/craftinators/barium/core/JobTransfer.java
+++ src/main/java/me/craftinators/barium/core/JobTransfer.java
@@ -7,6 +7,14 @@ import org.jetbrains.annotations.NotNull;
  */
 public record JobTransfer(Job oldJob, Job newJob) implements Comparable<JobTransfer> {
     /**
+     * Checks if this {@link JobTransfer} is redundant, meaning that the old job is the same as the new job.
+     * @return {@code true} if the old job is the same as the new job, {@code false} otherwise
+     */
+    public boolean isRedundant() {
+        return oldJob.equals(newJob);
+    }
+
+    /**
      * Compares this {@link JobTransfer} with the specified {@link JobTransfer} for order.
      * The comparison is based first on {@code oldJob}, and if they are equal, on {@code newJob}.
      *
