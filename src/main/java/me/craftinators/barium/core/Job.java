a40a35c4eac92b28c25cb0c10d86e8d7d86de96d
Craftinators <craftinatorssx@gmail.com>
2025-02-16 19:59:09 -0500

    feat(Job): add temporary jobs

--- src/main/java/me/craftinators/barium/core/Job.java
+++ src/main/java/me/craftinators/barium/core/Job.java
@@ -1,5 +1,11 @@
 package me.craftinators.barium.core;
 
 public enum Job {
-
+    TEMPORARY_JOB_1,
+    TEMPORARY_JOB_2,
+    TEMPORARY_JOB_3,
+    TEMPORARY_JOB_4,
+    TEMPORARY_JOB_5,
+    TEMPORARY_JOB_6,
+    TEMPORARY_JOB_7,
 }
